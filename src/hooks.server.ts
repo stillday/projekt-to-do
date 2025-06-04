6d74ddaa1445c8a118f5e6de783b343125e43905
JD <jan-dennisd@outlook.at>
2025-06-04 13:47:09 +0200

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,5 @@
 import type { Handle } from '@sveltejs/kit'
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
 
