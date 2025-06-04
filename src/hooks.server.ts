b94d04c83d4e2c7e455ed04727aa65993fb5db52
JD <jan-dennisd@outlook.at>
2025-06-04 13:53:50 +0200

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
 
+console.log('PUBLIC_SUPABASE_URL:', process.env.PUBLIC_SUPABASE_URL);
+console.log('PUBLIC_SUPABASE_ANON_KEY:', process.env.PUBLIC_SUPABASE_ANON_KEY);
+
+
 export const handle: Handle = async ({ event, resolve }) => {
 	const { data: { session } } = await supabase.auth.getSession()
 
